39217d7b9bbc65c0a113b5ee2d26cba4cf0dbcd3
新井ゆうな <araiyunaaa59@gmail.com>
2026-06-04 17:31:31 +0900

    Add files via upload

--- assets/script/toggle.js
+++ assets/script/toggle.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('.toggle');
-    const content = document.querySelector('.content');
+    document.querySelectorAll('.toggle').forEach(button => {
+        button.addEventListener('click', () => {
+            const content = button.nextElementSibling;
 
-    button.addEventListener('click', () => {
-        content.classList.toggle('is-open');
+            content.classList.toggle('is-open');
 
-        if (content.classList.contains('is-open')) {
-            button.textContent = 'CLOSE';
-        } else {
-            button.textContent = 'VIEW MORE';
-        }
+            button.textContent = content.classList.contains('is-open')
+                ? 'CLOSE'
+                : 'VIEW MORE';
+        });
     });
 });
